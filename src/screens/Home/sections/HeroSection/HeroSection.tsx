dc6c84aa7701b623643b75d66ef144462a880e8b
Maryam Yusuf <maryam.m.yusuf1802@gmail.com>
2025-06-28 18:34:47 +0100

    Updated HeroSection.tsx

--- src/screens/Home/sections/HeroSection/HeroSection.tsx
+++ src/screens/Home/sections/HeroSection/HeroSection.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent } from "../../../../components/ui/card";
 
 export const HeroSection = (): JSX.Element => {
   return (
-    <Card className="w-full border-b-0 rounded-[124px_124px_0px_0px] overflow-hidden mt-24">
+    <Card className="w-full rounded-[124px_124px_0px_0px] overflow-hidden mt-24">
       <CardContent className="flex flex-col items-center py-6 px-4">
         <div className="flex flex-col items-center max-w-[782px] mx-auto">
           <img
