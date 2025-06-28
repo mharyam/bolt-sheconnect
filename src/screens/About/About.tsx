1fe90ebef3c3cd5e7345be29fc47bebd510893b3
Maryam Yusuf <maryam.m.yusuf1802@gmail.com>
2025-06-29 00:21:24 +0100

    Updated About.tsx

--- src/screens/About/About.tsx
+++ src/screens/About/About.tsx
@@ -13,7 +13,6 @@ export const About = (): JSX.Element => {
       <NavigationSection />
       <AboutHeroSection />
       <AboutStatsSection />
-      <AboutCommunitySection />
       <FooterSection />
     </div>
   );
