339e0abec5d78d1c4b410ad6b1ce0a6acaf2f5a4
Maryam Yusuf <maryam.m.yusuf1802@gmail.com>
2025-06-28 19:41:43 +0100

    Speed up marquee animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -81,7 +81,7 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        marquee: "marquee 30s linear infinite",
+        marquee: "marquee 15s linear infinite",
       },
     },
     container: { center: true, padding: "2rem", screens: { "2xl": "1400px" } },
