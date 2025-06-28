f20177b5ee04d565b9c6399ced4bc38f69e3e910
Maryam Yusuf <maryam.m.yusuf1802@gmail.com>
2025-06-28 20:46:07 +0100

    Updated ArchiveHeroSection.tsx

--- src/screens/Archive/sections/ArchiveHeroSection/ArchiveHeroSection.tsx
+++ src/screens/Archive/sections/ArchiveHeroSection/ArchiveHeroSection.tsx
@@ -3,8 +3,6 @@ import React from "react";
 export const ArchiveHeroSection = (): JSX.Element => {
   return (
     <section className="w-full py-4 bg-neutralneutral-1">
-      <div className="max-w-[1360px] mx-auto px-10">
-      </div>
     </section>
   );
 };
