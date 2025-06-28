469e5b8851a6284664e8618ddde611c109c1de99
Maryam Yusuf <maryam.m.yusuf1802@gmail.com>
2025-06-28 20:15:39 +0100

    Updated CommunityEngagementWrapperSection.tsx

--- src/screens/Home/sections/CommunityEngagementWrapperSection/CommunityEngagementWrapperSection.tsx
+++ src/screens/Home/sections/CommunityEngagementWrapperSection/CommunityEngagementWrapperSection.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent } from "../../../../components/ui/card";
 
 export const CommunityEngagementWrapperSection = (): JSX.Element => {
   return (
-    <Card className="w-full bg-neutral-800 rounded-[64px] overflow-hidden py-24 px-8">
+    <Card className="w-full bg-neutral-800 rounded-[64px] overflow-hidden py-8 px-8">
       <CardContent className="flex flex-col items-center justify-center gap-12">
         <blockquote className="max-w-3xl mx-auto [font-family:'Lastik-Regular',Helvetica] font-normal text-mata-50 text-8xl text-center tracking-[-3.84px] leading-[115.2px]">
           "Alone we can do so little; together we can do so much."
