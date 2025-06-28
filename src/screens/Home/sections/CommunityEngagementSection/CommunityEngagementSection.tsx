1667d608049b6583ec5f4f2f7d4f15a242502725
Maryam Yusuf <maryam.m.yusuf1802@gmail.com>
2025-06-28 20:02:53 +0100

    Updated CommunityEngagementSection.tsx

--- src/screens/Home/sections/CommunityEngagementSection/CommunityEngagementSection.tsx
+++ src/screens/Home/sections/CommunityEngagementSection/CommunityEngagementSection.tsx
@@ -40,13 +40,6 @@ export const CommunityEngagementSection = (): JSX.Element => {
       <div className="container flex flex-row justify-between items-center gap-10">
         <Card className="w-[416px] rounded-2xl overflow-hidden bg-neutral-800 border-0">
           <div className="flex justify-center pt-7">
-        <div className="w-[114px] h-[41px] bg-mata-100 rounded flex items-center justify-center">
-          <img 
-            src="/mata-connect-logo.svg" 
-            alt="MATA CONNECT" 
-            className="h-6 w-auto"
-          />
-        </div>
 
           </div>
           <CardContent className="p-0 mt-6">
