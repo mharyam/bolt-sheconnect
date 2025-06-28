15ea32f9deb2643536d2083883ee31a9204a922b
Maryam Yusuf <maryam.m.yusuf1802@gmail.com>
2025-06-28 20:45:17 +0100

    Updated ArchiveHeroSection.tsx

--- src/screens/Archive/sections/ArchiveHeroSection/ArchiveHeroSection.tsx
+++ src/screens/Archive/sections/ArchiveHeroSection/ArchiveHeroSection.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 export const ArchiveHeroSection = (): JSX.Element => {
   return (
-    <section className="w-full py-4">
+    <section className="w-full py-4 bg-neutralneutral-1">
       <div className="max-w-[1360px] mx-auto px-10">
-        <div className="flex flex-col items-center text-center">
-        </div>
       </div>
     </section>
   );
