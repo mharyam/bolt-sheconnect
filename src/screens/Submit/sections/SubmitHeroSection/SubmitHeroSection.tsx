91f31322ae504972e424a1b9e9b276880430f877
Maryam Yusuf <maryam.m.yusuf1802@gmail.com>
2025-06-28 23:20:30 +0100

    Updated SubmitHeroSection.tsx

--- src/screens/Submit/sections/SubmitHeroSection/SubmitHeroSection.tsx
+++ src/screens/Submit/sections/SubmitHeroSection/SubmitHeroSection.tsx
@@ -5,7 +5,7 @@ export const SubmitHeroSection = (): JSX.Element => {
     <section className="w-full py-20 bg-neutralneutral-1">
       <div className="max-w-[1360px] mx-auto px-10">
         <div className="flex flex-col items-center text-center">
-          <p className="[font-family:'Lastik-Regular',Helvetica] font-normal text-neutral-800 text-[64px] tracking-[-2.56px] leading-[76.8px] mb-6">
+          <p className="[font-family:'Lastik-Regular',Helvetica] font-normal text-neutral-800 tracking-[-2.56px] leading-[76.8px] mb-6">
 Submit your(a) group and help more women connect, grow, and thrive.       
 </p>
         </div>
